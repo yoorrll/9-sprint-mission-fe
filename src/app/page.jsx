cadae7e82ed9bdec673eba2269bffdbc1f7c30e8
yoorrll <lyl1567@naver.com>
2025-11-10 22:33:02 +0900

    feat: 게시글 리스트 페이지(articles) 구현

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import PageLayout from "@/components/common/PageLayout";
+import ArticlePage from "./articles/page";
+
+export default function Home() {
+  return (
+    <PageLayout>
+      <ArticlePage />
+    </PageLayout>
+  );
+}
